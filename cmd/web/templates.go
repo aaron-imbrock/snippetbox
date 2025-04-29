96e6af52e2ab802691f0625dc23c325cebb5c844
Aaron Imbrock <aimbrock@gmail.com>
2025-04-29 13:52:20 -0700

    custom templating and helper functions are all working

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,20 +3,29 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/aaron-imbrock/snippetbox/internal/models"
 )
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 type templateData struct {
-	Snippet  models.Snippet
-	Snippets []models.Snippet
+	CurrentYear int
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
-
 	if err != nil {
 		return nil, err
 	}
@@ -24,17 +33,27 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		files := []string{
-			"./ui/html/base.html",
-			"./ui/html/partials/nav.html",
-			page,
-		}
-
-		ts, err := template.ParseFiles(files...)
+		// template.FuncMap must be registered w/ the template set before you
+		// call the ParseFiles() method. This means we have to use template.new() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
+		// Parse base template file into a template set.
+		// ts, err := template.ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
-
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
+		if err != nil {
+			return nil, err
+		}
+		// Call ParseFiles() *on this templae set* to add the page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		// Add the template set to the map as normal...
 		cache[name] = ts
 	}
 
